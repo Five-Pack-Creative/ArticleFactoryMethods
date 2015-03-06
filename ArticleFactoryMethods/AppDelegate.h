0515c4f63a0cc9368d6121f44ff2a46ae5c50056
Jerry Beers <Jerry@fivepackcreative.com>
2015-03-06 09:45:55 -0600

    Initial Commit

---
+++ ArticleFactoryMethods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ArticleFactoryMethods
+//
+//  Created by Jerry Beers on 3/6/15.
+//  Copyright (c) 2015 fivepackcreative.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ArticleFactoryMethods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ArticleFactoryMethods
+//
+//  Created by Jerry Beers on 3/6/15.
+//  Copyright (c) 2015 fivepackcreative.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
